b9e6ed5b203eb2e86e256cd284d039585c0fdc7d
Valerio Formato <valerio.formato@cern.ch>
2022-04-02 12:10:34 +0200

    Move math code in `utils` subdirectory and add `Sumo` namespace

---
+++ src/utils/math.hpp
@@ -0,0 +1,31 @@
+#ifndef SUMO_MATH_HPP
+#define SUMO_MATH_HPP
+
+#include <cmath>
+#include <concepts>
+
+namespace Sumo {
+template<typename T>
+concept Numeric = std::integral<T> || std::floating_point<T>;
+
+[[nodiscard]] constexpr auto pow2(Numeric auto x) { return x * x; }
+
+[[nodiscard]] constexpr auto
+  in_range(Numeric auto const x, Numeric auto const range_start, Numeric auto const range_end)
+{
+  return x >= a && x <= b;
+}
+
+[[nodiscard]] constexpr auto clamp(Numeric auto const x, Numeric auto const range_start, Numeric auto const range_end)
+{
+  if (x > b) return b;
+  if (x < a) return a;
+  return x;
+}
+
+template<Numeric T> [[nodiscard]] constexpr auto wrap_increment(T const x, T const range_start, T const range_end)
+{
+  return x + static_cast<T>(1) >= b ? a : x + static_cast<T>(1);
+}
+}// namespace Sumo
+#endif
---
+++ src/utils/vec2.hpp
@@ -0,0 +1,75 @@
+#ifndef SUMO_VEC2_HPP
+#define SUMO_VEC2_HPP
+
+#include "utils/math.hpp"
+
+template<Numeric T> struct vec2
+{
+  T x, y;
+
+  vec2() = default;
+  constexpr vec2(T x, T y) : x{ x }, y{ y } {}
+  explicit constexpr vec2(T k) : x{ k }, y{ k } {}
+
+  [[nodiscard]] bool constexpr operator==(vec2<T> const &rhs) const = default;
+
+  vec2 constexpr &operator+=(vec2<T> const &rhs)
+  {
+    x += rhs.x, y += rhs.y;
+    return *this;
+  }
+  vec2 constexpr &operator*=(T const k)
+  {
+    x += k, y *= k;
+    return *this;
+  }
+};
+
+template<Numeric T> [[nodiscard]] vec2<T> constexpr operator-(vec2<T> const &rhs) { return { -rhs.x, -rhs.y }; }
+
+template<Numeric T> [[nodiscard]] vec2<T> constexpr operator*(vec2<T> const &lhs, T k)
+{
+  return { lhs.x * k, lhs.y * k };
+}
+
+template<Numeric T> [[nodiscard]] vec2<T> constexpr operator*(T k, vec2<T> const &rhs)
+{
+  return { rhs.x * k, rhs.y * k };
+}
+
+template<Numeric T> [[nodiscard]] vec2<T> constexpr operator+(vec2<T> const &lhs, vec2<T> const &rhs)
+{
+  return { lhs.x + rhs.x, lhs.y + rhs.y };
+}
+
+template<Numeric T> [[nodiscard]] vec2<T> constexpr operator-(vec2<T> const &lhs, vec2<T> const &rhs)
+{
+  return { lhs.x - rhs.x, lhs.y - rhs.y };
+}
+
+template<Numeric T> [[nodiscard]] T constexpr operator^(vec2<T> const &lhs, vec2<T> const &rhs)
+{
+  return lhs.x * rhs.y - lhs.y * rhs.x;
+}
+
+template<std::floating_point T> [[nodiscard]] T constexpr length(vec2<T> const &v)
+{
+  return std::sqrt(pow2(v.x) + pow2(v.y));
+}
+
+template<std::floating_point T> [[nodiscard]] T constexpr distance(vec2<T> const &lhs, vec2<T> const &rhs)
+{
+  return std::sqrt(pow2(lhs.x - rhs.x) + pow2(lhs.y - rhs.y));
+}
+
+template<std::floating_point T> [[nodiscard]] vec2<T> constexpr normalize(vec2<T> const &v)
+{
+  auto const l = length(v);
+  return { v.x / l, v.y / l };
+}
+
+using vec2f = vec2<float>;
+using vec2i = vec2<int>;
+using vec2u = vec2<unsigned>;
+
+#endif// VEC2_HPP
