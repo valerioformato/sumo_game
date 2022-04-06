6aea4c1742bd8c84b4dcaddd06c755a194dc1ab4
Valerio Formato <valerio.formato@cern.ch>
2022-04-06 22:57:06 +0200

    Re-add CLI (expected from tests)

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 // dependencies headers
 #include <docopt/docopt.h>
+#include <fmt/format.h>
 #include <ftxui/component/captured_mouse.hpp>// for ftxui
 #include <ftxui/component/component.hpp>// for Slider
 #include <ftxui/component/screen_interactive.hpp>// for ScreenInteractive
@@ -21,6 +22,25 @@
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 {
+  static constexpr auto USAGE =
+    R"(intro
+    Usage:
+          intro
+          intro (-h | --help)
+          intro --version
+ Options:
+          -h --help     Show this screen.
+          --version     Show version.
+)";
+
+  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+    { std::next(argv), std::next(argv, argc) },
+    true,// show help if requested
+    fmt::format("{} {}",
+      sumo_game::cmake::project_name,
+      sumo_game::cmake::project_version));// version string, acquired
+                                          // from config.hpp via CMake
+
   Sumo::GameEngine engine;
   engine.run();
 }
